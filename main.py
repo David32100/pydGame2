a3047dea00c1d684c1f84a239bca0460f1777a2d
david321 <davidkalmangross@gmail.com>
2024-02-14 19:23:19 -0500

    Realized typing NOTE makes it turn blue

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # To do: jumper.py Ln All Introduce acceleration for jumping and falling, jumper.py Ln 112 Fix message initiated jummping main.py Ln 45 Finish party screen, None Ln None None Fix joining games with a party, Ln None Make text chat, main.py Ln 47 Make settings screen, None Ln None Make accounts and account screen, None Ln None Add SFX and music, None Ln None Add graphics and transitions, None Ln None Polish game, Final step: Setup and share the new game!!!
 # What to send to server: Status
 # Actions to send to server: talk: username, position (+ scroll), lobby, text status: username, status
+# NOTE: Notes light up blue, and usernames will have a max of 16 characters!!!NOTE NOTE NOTE NOTE NOTE NOTE
+"""NOTE: NOTE"""
 
 import pygame
 import threading
